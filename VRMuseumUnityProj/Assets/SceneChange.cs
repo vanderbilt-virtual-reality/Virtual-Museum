2ac509cb015e5a2f79b945e3904f44635d93ade2
YutaoCao <55848310+YutaoCao@users.noreply.github.com>
2019-10-30 17:10:17 -0500

    Added scene changing

---
+++ VRMuseumUnityProj/Assets/SceneChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCount);
+        }
+    }
+}
