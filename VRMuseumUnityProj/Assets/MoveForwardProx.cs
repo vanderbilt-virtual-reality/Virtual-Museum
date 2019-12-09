3b050b8b51d241b7c4e24cd7ea26015a5f576195
kokolajj <john.j.kokolakis@vanderbilt.edu>
2019-12-08 23:14:36 -0600

    Final scripting and aesthetics

---
+++ VRMuseumUnityProj/Assets/MoveForwardProx.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForwardProx : MonoBehaviour
+{
+    public GameObject museumObject;
+    public GameObject displayObject;
+    public float proxDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, museumObject.transform.position) < proxDistance)
+        {
+            displayObject.transform.position = new Vector3(25, (float) 2.63, -2);
+        }
+        else if (Vector3.Distance(this.transform.position, museumObject.transform.position) > proxDistance)
+        {
+            displayObject.transform.position = new Vector3(27, (float)2.63, -2);
+        }
+    }
+}
